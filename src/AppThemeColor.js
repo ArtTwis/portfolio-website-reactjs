658e0baae242d641be980ca3abebe3f19ef89b26
Twinkle Goyal <twinkle.goyal@intimetec.com>
2022-10-19 23:52:33 +0530

    add new color in theme file

--- src/AppThemeColor.js
+++ src/AppThemeColor.js
@@ -3,6 +3,7 @@ const AppThemeColor = {
     bodyBgColor: '#202124',
     primaryColor: '#a1ff1a',
     secondaryColor: '#181818',
+    techCardBG: '#363636',
     copyrightBar: '#141414',
     primaryColorGradiant:
       '-webkit-linear-gradient( 0deg, #9dfd19 20%, #a1ff1a 60%, #00000003 100% )',
@@ -10,12 +11,13 @@ const AppThemeColor = {
     activeTextColor: '#000000',
     commonLightColor: '#ffffff',
     commonDarkColor: '#000000',
-    cardBorderColor: '#444444',
+    cardBorderColor: '#858585',
   },
   light: {
     bodyBgColor: '#EAEBD5',
     primaryColor: '#13bb00',
     secondaryColor: '#DCDEBA',
+    techCardBG: '#C6CA91',
     copyrightBar: '#CED19F',
     primaryColorGradiant:
       '-webkit-linear-gradient( 0deg, #13bb00 20%, #13bb00c8 60%, #ffffff03 100% )',
