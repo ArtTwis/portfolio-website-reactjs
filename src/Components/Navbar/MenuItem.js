c48d408e34739537979e839ed8f7dcd9f9a4c597
Twinkle Goyal <twinkle.goyal@intimetec.com>
2022-10-19 23:52:54 +0530

    modify navigation menu item

--- src/Components/Navbar/MenuItem.js
+++ src/Components/Navbar/MenuItem.js
@@ -1,32 +1,32 @@
 export const MenuItem = [
-    {
-        title: 'Home',
-        url: '/',
-        cname: 'nav-link',
-    },
-    {
-        title: 'Education',
-        url: '/education',
-        cname: 'nav-link',
-    },
-    {
-        title: 'Experience',
-        url: '/experience',
-        cname: 'nav-link',
-    },
-    {
-        title: 'Projects',
-        url: '/projects',
-        cname: 'nav-link',
-    },
-    {
-        title: 'Blog',
-        url: '/blogs',
-        cname: 'nav-link',
-    },
-    {
-        title: 'Contact Me',
-        url: '/contactme',
-        cname: 'nav-link',
-    },
+  {
+    title: 'Home',
+    url: '/',
+    cname: 'nav-link',
+  },
+  {
+    title: 'My Story',
+    url: '/mystory',
+    cname: 'nav-link',
+  },
+  {
+    title: 'Career',
+    url: '/career',
+    cname: 'nav-link',
+  },
+  {
+    title: 'Projects',
+    url: '/projects',
+    cname: 'nav-link',
+  },
+  {
+    title: 'Blog',
+    url: '/blogs',
+    cname: 'nav-link',
+  },
+  {
+    title: 'Contact Me',
+    url: '/contactme',
+    cname: 'nav-link',
+  },
 ];
