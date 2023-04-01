97f7f7bc5d8372743dde0c66bde581dee285e6f2
twinkle-goyal <twinkle.goyal@hp.com>
2023-04-01 21:42:12 +0530

    added my image and new route to myStory

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Base from './Base';
-import Home from './HomePage/Home';
-import { createStore } from 'redux';
-import rootReducer from './reducers';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Base from "./Base";
+import Home from "./Pages/HomePage/Home";
+import Story from "./Pages/MyStory/Story";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
 
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
     <Router>
       <Routes>
         <Route path='/' exact element={<Home />} />
-        <Route path='/education' element={<Base />} />
+        <Route path='/mystory' element={<Story />} />
         <Route path='/experience' element={<Base />} />
         <Route path='/projects' element={<Base />} />
         <Route path='/blogs' element={<Base />} />
