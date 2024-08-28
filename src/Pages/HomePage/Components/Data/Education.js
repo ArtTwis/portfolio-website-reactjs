1ff977577255ff643b266427b9e0040693ff03a1
Art_Twis <34947496+ArtTwis@users.noreply.github.com>
2024-08-28 17:38:14 +0530

    update resume link and minor experience data

--- src/Pages/HomePage/Components/Data/Education.js
+++ src/Pages/HomePage/Components/Data/Education.js
@@ -1,21 +1,21 @@
 export const Education = [
   {
-    institute: 'Lovely Professional University',
-    city: 'Jalandhar, Punjab',
-    course: 'Master of Computer Application ( MCA )',
-    period: '2018-2021',
+    institute: "Lovely Professional University",
+    city: "Jalandhar, Punjab",
+    course: "Master of Computer Application ( MCA )",
+    period: "2018-2021",
   },
   {
-    institute: 'Rajasthan College for Higher Education',
-    city: 'Hanumangarh, Rajasthan',
-    course: 'Bachelor of Computer Application ( BCA )',
-    period: '2013-2018',
+    institute: "Rajasthan College for Higher Education",
+    city: "Hanumangarh, Rajasthan",
+    course: "Bachelor of Computer Application ( BCA )",
+    period: "2013-2018",
   },
   {
-    institute: 'Star Plus Convent School',
-    city: 'Raman Mandi, Punjab',
-    course: 'High School ( CBSE )',
-    period: '2011-2013',
+    institute: "Star Plus Convent School",
+    city: "Raman Mandi, Punjab",
+    course: "High School ( CBSE )",
+    period: "2011-2013",
   },
   // {
   //   institute: 'Swami Keshwanand Memorial Public School',
--- src/Pages/HomePage/Components/Data/Experience.js
+++ src/Pages/HomePage/Components/Data/Experience.js
@@ -3,30 +3,22 @@ export const Experience = [
     title: "Software Engineer",
     organization: "In Time Tec Visionsoft Pvt. Ltd.",
     location: "Jaipur (Rajasthan)",
-    period: "Sep 2021 - Present, 2 Year 8 Month",
+    period: "Sep 2021 - Present, 2 Year 10 Month",
     projects: [
       {
         client: "NextGen Ag-Tech",
         module: "Roll On Dispatch",
         description:
-          "Designed and developed full-stack application which manages stops and pickups and connects dispatch to drivers",
-        keyword: "ReactJs, TypeScript, .NET Core Web API’s, MySql",
+          "Designed and developed frontend application which manages stops and pickups and connects dispatch to drivers",
+        keyword: "ReactJs, TypeScript",
       },
       {
         client: "Hewlett-Packard ( HP )",
         module: "Net Promo Framework Tool",
         description:
-          "Designed and developed application front-end for internal HP users to compare sales/stocks records of different SKU for different past quarters",
+          "Designed and developed full-stack application for internal HP users to compare sales/stocks records of different SKU for different past quarters",
         keyword:
-          "ReactJS, Ag-Grid, HighCharts, TypeScript, Styled-Component, Redux-Toolkit, Redux-Saga",
-      },
-      {
-        client: "Truckstop",
-        module: "Loadpay",
-        description:
-          "Worked in Loadpay squad where I mantain all pay related services of Truckstop",
-        keyword:
-          ".NET MVC Framework, SQL, Elastic Search, NodeJS, AWS, Postman, Gitlab, GoJira",
+          "ReactJS, Ag-Grid, HighCharts, TypeScript, Styled-Component, Redux-Toolkit, Express.js",
       },
     ],
   },
@@ -34,12 +26,13 @@ export const Experience = [
     title: "Assistant Developer",
     organization: "Sun-Soft Technologies Pvt. Ltd.",
     location: "Bathinda (Punjab)",
-    period: "June 2019 - Sep 2021, 2 Years 3 Month",
+    period: "June 2019 - Sep 2021, 1 Years 3 Month",
     projects: [
       {
         client: "Jetmanlabs",
         module: "Jetman",
-        description: "Build a tool for API Testing, worked on Runner Module, build Analytics dashboard using Web-Charts, API Marketplace, CLI Application tool",
+        description:
+          "Build a tool for API Testing, worked on Runner Module, build Analytics dashboard using Web-Charts, API Marketplace, CLI Application tool",
         keyword:
           "Core-Javascript, Node.Js, Electron.Js, MongoDB, NeDB, Elastic Search, C3.Js, Commander.Js",
       },
